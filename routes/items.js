baae97862d3434f93f7d9a1f41828b6b43927510
Quentin Ikeno <ikenoq@gmail.com>
2022-05-08 18:09:14 -1000

    Add post route for items.

--- routes/items.js
+++ routes/items.js
@@ -7,4 +7,17 @@ router.get("/", (req, res) => {
 	return res.json({ items });
 });
 
+router.post("/", (req, res, next) => {
+	try {
+		const { name, price } = req.body;
+		if (!name) throw new ExpressError("Name is required", 400);
+		if (!price) throw new ExpressError("Price is required", 400);
+		const newItem = { name, price };
+		items.push(newItem);
+		return res.status(201).json({ item: newItem });
+	} catch (error) {
+		next(error);
+	}
+});
+
 module.exports = router;
